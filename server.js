a943f69b6af52705525e77c07903971a5344e38e
Gabriel Rossetto <gabriel.rossetto.santos@sap.com>
2020-08-11 21:14:00 -0300

    first api test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors())
 
 app.get('/api/mensagem', (req, res) => {
     res.send({ express: 'Hello From Express' });
